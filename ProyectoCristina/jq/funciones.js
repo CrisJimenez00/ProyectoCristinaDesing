9f4acca208b9826752e398adf0ca5c9b95897fdb
Cristina Jiménez <92090553+CrisJimenez00@users.noreply.github.com>
2023-04-22 17:57:17 +0000

    queda por mejorarlo

--- ProyectoCristina/jq/funciones.js
+++ ProyectoCristina/jq/funciones.js
@@ -1,121 +1,68 @@
 //Para slide
-/*$(document).ready(function () {
-  var SliderModule = (function () {
-    var pb = {}; //Creamos un objeto
-    //Almacenamos nuestro #slider en el atributo elslider.
-    pb.elslider = $("#slider");
-    //El atributo item es un array que almacena los paneles del slider
-    pb.items = {
-      panels: pb.elslider.find(".slider-wrapper > li"),
-    };
+$(document).ready(function(){
+	var imgItems = $('.slider li').length; // Numero de Slides
+	var imgPos = 1;
 
-    // Interval del Slider
-    var SliderInterval,
-      currentSlider = 0,
-      nextSlider = 1,
-      lengthSlider = pb.items.panels.length;
+	// Agregando paginacion --
+	for(i = 1; i <= imgItems; i++){
+		$('.pagination').append('<li><span class="fa fa-circle"></span></li>');
+	} 
+	//------------------------
 
-    // Constructor del Slider
-    pb.init = function (settings) {
-      this.settings = settings || { duration: 5000 };
-      var loscontroles = "";
-      //console.log('Inicializado');
-      SliderInit(); //Para inicializar el slider
+	$('.slider li').hide(); // Ocultanos todos los slides
+	$('.slider li:first').show(); // Mostramos el primer slide
+	$('.pagination li:first').css({'color': '#CD6E2E'}); // Damos estilos al primer item de la paginacion
 
-      //Creamos los controles del Slider en tiempo de ejecución
-      for (var i = 0; i < lengthSlider; i++) {
-        if (i == 0) {
-          loscontroles += '<li class="active"></li>';
-        } else {
-          loscontroles += "<li></li>";
-        }
-      }
-      //console.log(loscontroles);
+	// Ejecutamos todas las funciones
+	$('.pagination li').click(pagination);
+	$('.right span').click(nextSlider);
+	$('.left span').click(prevSlider);
 
-      //Insertamos los controles creados en el HTML
-      $("#control-buttons").html(loscontroles);
 
-      $("#control-buttons li").click(function () {
-        //Al hacer clic vemos en la consola el índice
-        //console.log($(this).index());
-        //console.log(currentSlider);
-        console.log($(this).index());
-        if (currentSlider !== $(this).index()) {
-          cambiarPanel($(this).index());
-        }
-      });
-    };
+	setInterval(function(){
+		nextSlider();
+	}, 4000);
 
-    //Función que inicializar el slider
-    var SliderInit = function () {
-      SliderInterval = setInterval(pb.startSlider, pb.settings.duration);
-    };
+	// FUNCIONES =========================================================
 
-    pb.startSlider = function () {
-      var paneles = pb.items.panels;
-      var controles = $("#control-buttons li");
+	function pagination(){
+		var paginationPos = $(this).index() + 1; // Posicion de la paginacion seleccionada
 
-      //Controlamos si nos encontramos en el último panel
-      if (nextSlider >= lengthSlider) {
-        nextSlider = 0;
-        currentSlider = lengthSlider - 1;
-      }
-      //EFECTOS
-      //Eliminamos la clase en todos los controles
-      controles.removeClass("active");
-      //Añadimos la clase al control del panel seleccionado
-      controles.eq(nextSlider).addClass("active");
-      //Efectos de transición
-      paneles.eq(currentSlider).fadeOut("slow");
-      paneles.eq(nextSlider).fadeIn("slow");
+		$('.slider li').hide(); // Ocultamos todos los slides
+		$('.slider li:nth-child('+ paginationPos +')').fadeIn(); // Mostramos el Slide seleccionado
 
-      //Actualizamos las variables
-      currentSlider = nextSlider;
-      nextSlider += 1;
-    };
+		// Dandole estilos a la paginacion seleccionada
+		$('.pagination li').css({'color': '#858585'});
+		$(this).css({'color': '#CD6E2E'});
 
-    //Función para los controles del Slider
-    //Recibe el índice del panel a mostrar
-    var cambiarPanel = function (indice) {
-      //Limpiar el intervalo previamente.
-      clearInterval(SliderInterval);
+		imgPos = paginationPos;
 
-      var paneles = pb.items.panels;
-      var controles = $("#control-buttons li");
-      //Comprobamos que el índice esté disponible
-      //dentro de los paneles del slider.
-      if (indice >= lengthSlider) {
-        indice = 0; //Para que no se pase de la cantidad de imágenes
-      } else if (indice < 0) {
-        indice = lengthSlider - 1;
-      }
-      //EFECTOS
-      //Eliminamos la clase en todos los controles
-      controles.removeClass("active");
-      //Añadimos la clase al control del panel seleccionado
-      controles.eq(indice).addClass("active");
-      //Efectos de transición
-      paneles.eq(currentSlider).fadeOut("slow");
-      //El siguiente panel del slider es el que indique
-      //el parámetro "indice"
-      paneles.eq(indice).fadeIn("slow");
+	}
 
-      //Actualizamos los datos
-      currentSlider = indice;
-      nextSlider = indice + 1;
+	function nextSlider(){
+		if( imgPos >= imgItems){imgPos = 1;} 
+		else {imgPos++;}
 
-      //Reactivar el Slider
-      SliderInit();
-    };
+		$('.pagination li').css({'color': '#858585'});
+		$('.pagination li:nth-child(' + imgPos +')').css({'color': '#CD6E2E'});
 
-    return pb; //Devolvemos el objeto pb
-  })(); //()) es para que se ejecute automáticamente.
+		$('.slider li').hide(); // Ocultamos todos los slides
+		$('.slider li:nth-child('+ imgPos +')').fadeIn(); // Mostramos el Slide seleccionado
 
-  //SliderModule.init({duration:2000});
-  //Llamada al constructor
-  SliderModule.init();
-});*/
+	}
 
+	function prevSlider(){
+		if( imgPos <= 1){imgPos = imgItems;} 
+		else {imgPos--;}
+
+		$('.pagination li').css({'color': '#858585'});
+		$('.pagination li:nth-child(' + imgPos +')').css({'color': '#CD6E2E'});
+
+		$('.slider li').hide(); // Ocultamos todos los slides
+		$('.slider li:nth-child('+ imgPos +')').fadeIn(); // Mostramos el Slide seleccionado
+	}
+
+});
 //MENÚ
 $(document).ready(function () {
   $("div.menu").hover(function (e) {
